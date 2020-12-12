f1b6a2fd98db6242b5c5f8251e07fd6cb011ecd3
Teedari <godfredderi47@gmail.com>
2020-12-12 12:18:20 -0800

    added persistence storage

--- flutter_dark_mode_app/lib/main.dart
+++ flutter_dark_mode_app/lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dark_mode_app/theme_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'services/theme_create_with_provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
   runApp(
     ChangeNotifierProvider<CustomTheme>(
-      create: (context) => CustomTheme(),
+      create: (context) => CustomTheme(isDark: prefs.getBool('isDark')),
       builder: (context, _) => MyAppBase(),
     ),
   );
